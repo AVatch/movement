147de7b918141fac344b3a3228eca06399a59c6c
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-02-22 17:37:50 -0500

    basic ionic tab boilerplate

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('starter.controllers', [])
+
+.controller('VenuesCtrl', function($scope) {})
+
+.controller('VenuesDetailCtrl', function($scope, $stateParams) {
+  
+})
+
+.controller('ActivityCtrl', function($scope) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+})
+
+.controller('SettingsCtrl', function($scope) {
+  
+})
+
+.controller('AboutCtrl', function($scope) {
+  
+});
