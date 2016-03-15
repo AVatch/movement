1b94763c5840e3429bdb10b00a3adf7a870e8202
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-03-15 15:58:48 -0400

    using a new version of gmaps

--- www/js/app.js
+++ www/js/app.js
@@ -39,7 +39,7 @@ angular.module('movement', ['ionic',
 .config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
         key: 'AIzaSyBiTGJkXnuwIHMbg0keDrhP--hBClpNMAA', // yolo
-        v: '3.20', //defaults to latest 3.X anyhow
+        v: '3.22', //defaults to latest 3.X anyhow
         libraries: 'weather,geometry,visualization,marker'
     });
 })
