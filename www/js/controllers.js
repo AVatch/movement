76055ef63e7706a3d23d6ae59cae59aa27967341
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-03-16 11:38:38 -0400

    event listener on view enter

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -141,25 +141,28 @@ angular.module('movement.controllers', [])
             ]
         });
     };
-    if(!GeoTracking.isTrackingEnabled()){
-        showTrackingPermissionPopup()
-            .then(function(){
-                GeoTracking.startBGGeoTracking()
-            });
-    }else{
-        // center map on coords
-        console.log("centering map");
-        GeoTracking.getCurrentCoords()
-            .then(function(location){
-                // center map
-                $scope.mapObj.center.latitude = location.coords.latitude;
-                $scope.mapObj.center.longitude = location.coords.longitude;
-                
-                // render you are here pin
-                $scope.meMarker.center.latitude = location.coords.latitude;
-                $scope.meMarker.center.longitude = location.coords.longitude;
-            })  
-    }
+    
+    $scope.$on('$ionicView.enter', function(e) {
+        if(!GeoTracking.isTrackingEnabled()){
+            showTrackingPermissionPopup()
+                .then(function(){
+                    GeoTracking.startBGGeoTracking()
+                });
+        }else{
+            // center map on coords
+            console.log("centering map");
+            GeoTracking.getCurrentCoords()
+                .then(function(location){
+                    // center map
+                    $scope.mapObj.center.latitude = location.coords.latitude;
+                    $scope.mapObj.center.longitude = location.coords.longitude;
+                    
+                    // render you are here pin
+                    $scope.meMarker.center.latitude = location.coords.latitude;
+                    $scope.meMarker.center.longitude = location.coords.longitude;
+                })  
+        }
+    });
     
 })
 
